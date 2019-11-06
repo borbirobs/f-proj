c491b36d1d3cb1f2dc2f9d8f4d0f07905c5265ca
Sara Borbiro <saraborbiro@gmail.com>
2019-11-06 18:20:45 +0100

    elsoteszt

---
+++ modify.py
@@ -0,0 +1,16 @@
+import time
+from test.selenium import webdriver
+
+driver = webdriver.Chrome('/usr/local/bin/chromedriver')  # Optional argument, if not specified will search path.
+driver.get('http://www.learnwebservices.com/locations/server');
+ts = time.time()
+name = "bb" + str(ts)
+driver.find_element_by_name("name").send_keys(name)
+driver.find_element_by_name("coords").send_keys("10,10")
+button = driver.find_element_by_xpath("//html/body/div/form/button")
+button.click()
+driver.find_element_by_xpath("//tr[td[text() ='${name}']]//a").click()
+
+time.sleep(4)
+
+driver.quit()
---
+++ test/test_locations.py
@@ -0,0 +1,14 @@
+import time
+from test.selenium import webdriver
+
+driver = webdriver.Chrome('/usr/local/bin/chromedriver')  # Optional argument, if not specified will search path.
+driver.get('http://www.learnwebservices.com/locations/server');
+driver.find_element_by_name("name").send_keys("")
+driver.find_element_by_name("coords").send_keys("10,10")
+button = driver.find_element_by_xpath("//html/body/div/form/button")
+button.click()
+assert "must not be blank" in driver.page_source
+
+time.sleep(4)
+
+driver.quit()
