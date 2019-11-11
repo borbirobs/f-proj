593de5da7b86dd7400630d910793993510fb78c1
Sara Borbiro <saraborbiro@gmail.com>
2019-11-11 15:19:08 +0100

    testtri

--- docs/cica/test-triangle.py
+++ docs/cica/test-triangle.py
@@ -8,22 +8,17 @@ driver.get('https://borbirobs.github.io/f-proj/cica/triangles.html')
 
 
 def szamol(a, b, c):
-        a_input = driver.find_element(By.ID, "a-input");
-        a_input.send_keys(a);
-
-        b_input = driver.find_element(By.ID, "b-input");
-        b_input.send_keys(b);
-
-
-        c_input = driver.find_element(By.ID, "c-input");
-        c_input.send_keys(c);
-
-        calculate_button = driver.find_element(By.ID, "calculate-button");
-        calculate_button.click();
-
-        li_text = driver.find_element_by_xpath("//li[last()]").text
-        print(li_text)
-        assert li_text == "a = 1, b = 1, c = 1: Egyenlő oldalú"
-        driver.save_screenshot("result.png")
+  a_input = driver.find_element(By.ID, "a-input");
+  a_input.send_keys(a);
+  b_input = driver.find_element(By.ID, "b-input");
+  b_input.send_keys(b);
+  c_input = driver.find_element(By.ID, "c-input");
+  c_input.send_keys(c);
+  calculate_button = driver.find_element(By.ID, "calculate-button");
+  calculate_button.click();
+  li_text = driver.find_element_by_xpath("(/html/body/form/ul/li)[last()]").text
+  print(li_text)
+  assert li_text == "a = 1, b = 1, c = 1: Egyenlő oldalú"
+  driver.save_screenshot("result.png")
 
 szamol(1,1,1)
