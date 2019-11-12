eea6827ff94e2deb6d3c952aaff72f58000cf92a
Sara Borbiro <saraborbiro@gmail.com>
2019-11-12 13:14:46 +0100

    ezmostmárműködik!

--- docs/cica/test-triangle.py
+++ docs/cica/test-triangle.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
+
+import time
 
 
 driver = webdriver.Chrome('/usr/local/bin/chromedriver')
@@ -9,24 +10,33 @@ driver.get('https://borbirobs.github.io/f-proj/cica/triangles.html')
 
 
 
-def szamol(a, b, c):
+def szamol(a, b, c, triangletype):
   a_input = driver.find_element(By.ID, "a-input");
   a_input.send_keys(a);
   b_input = driver.find_element(By.ID, "b-input");
   b_input.send_keys(b);
   c_input = driver.find_element(By.ID, "c-input");
   c_input.send_keys(c);
-  element = driver.find_element(By.ID, "calculate-button")
-  actions = ActionChains(driver)
-  actions.move_to_element(element).click_and_hold().perform()
-  element = driver.find_element(By.CSS_SELECTOR, "html")
-  actions = ActionChains(driver)
-  actions.move_to_element(element).release().perform()
-  driver.find_element(By.CSS_SELECTOR, "html").click()
-  driver.find_element(By.ID, "calculate-button").click()
+  calculate_button = driver.find_element(By.ID, "calculate-button")
+  calculate_button.click()
+  calculate_button.click()
+
   li_text = driver.find_element_by_xpath("//ul[@id='result-ul']/li[last()]").text
   print(li_text)
-  assert li_text == "a = 1, b = 1, c = 1: Egyenlő oldalú"
-  driver.save_screenshot("result.png")
 
-szamol(1,1,1);
+  expectedtext = "a = "+str(a)+", b = "+str(b)+", c = "+str(c)+": "+triangletype
+  assert li_text == expectedtext
+  driver.save_screenshot("result.png")
+  # driver.refresh()
+
+  a_input.clear()
+  b_input.clear()
+  c_input.clear()
+
+
+
+szamol(1,1,1, "Egyenlő oldalú")
+szamol(1,2,3, "Nem háromszög")
+# szamol(8,5,5, "Egyenlő szárú")
+# Az egyenlő szárú háromszöget is egyenlő oldalúnak számolja. ITT A HIBA!
+szamol(8,6,5, "Általános")
